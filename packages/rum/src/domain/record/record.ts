e9ac545e29371f1b97a43615194b25004f90ef93
Fiona <baiyang.feng@outlook.com>
2025-07-07 11:52:57 +0800

    feat: support replay data collector with rrweb.

--- packages/rum/src/domain/record/record.ts
+++ packages/rum/src/domain/record/record.ts
@@ -1,27 +1,8 @@
-import { sendToExtension } from '@flashcatcloud/browser-core'
+// eslint-disable-next-line
+import { record as rrwebRecord } from '@rrweb/record'
 import type { LifeCycle, RumConfiguration, ViewHistory } from '@flashcatcloud/browser-rum-core'
+import { addTelemetryError, DefaultPrivacyLevel } from '@flashcatcloud/browser-core'
 import type { BrowserRecord } from '../../types'
-import * as replayStats from '../replayStats'
-import type { Tracker } from './trackers'
-import {
-  trackFocus,
-  trackFrustration,
-  trackInput,
-  trackMediaInteraction,
-  trackMouseInteraction,
-  trackMove,
-  trackMutation,
-  trackScroll,
-  trackStyleSheet,
-  trackViewEnd,
-  trackViewportResize,
-  trackVisualViewportResize,
-} from './trackers'
-import { createElementsScrollPositions } from './elementsScrollPositions'
-import type { ShadowRootsController } from './shadowRootsController'
-import { initShadowRootsController } from './shadowRootsController'
-import { startFullSnapshots } from './startFullSnapshots'
-import { initRecordIds } from './recordIds'
 
 export interface RecordOptions {
   emit?: (record: BrowserRecord) => void
@@ -29,71 +10,66 @@ export interface RecordOptions {
   lifeCycle: LifeCycle
   viewHistory: ViewHistory
 }
-
 export interface RecordAPI {
   stop: () => void
   flushMutations: () => void
-  shadowRootsController: ShadowRootsController
 }
 
-export function record(options: RecordOptions): RecordAPI {
-  const { emit, configuration, lifeCycle } = options
-  // runtime checks for user options
+export function record(options: RecordOptions) {
+  const { emit, configuration } = options
+  const { defaultPrivacyLevel } = configuration
+
   if (!emit) {
-    throw new Error('emit function is required')
+    addTelemetryError(new Error('emit function is required'), { context: 'rrweb-record' })
   }
+  const stop = rrwebRecord({
+    emit: (record) => {
+      emit?.(record as unknown as BrowserRecord)
+    },
+    sampling: {
+      scroll: 200,
+      media: 800,
+      mouseInteraction: {
+        mouseUp: false,
+        mouseDown: false,
+        ContextMenu: false,
+        Blur: false,
+        Click: true,
+      },
+      input: 'last',
+    },
+    errorHandler: (error) => {
+      addTelemetryError(error, { context: 'rrweb-record' })
+    },
+    recordCanvas: false,
+    maskAllInputs: defaultPrivacyLevel !== DefaultPrivacyLevel.ALLOW,
+    maskInputOptions: {
+      email: true,
+      tel: true,
+      password: true,
+    },
+    maskTextFn: (text) => (defaultPrivacyLevel === DefaultPrivacyLevel.MASK ? '********' : text),
+    checkoutEveryNms: 3 * 60 * 1000,
+  })
 
-  const emitAndComputeStats = (record: BrowserRecord) => {
-    emit(record)
-    sendToExtension('record', { record })
-    const view = options.viewHistory.findView()!
-    replayStats.addRecord(view.id)
+  const flushMutations = () => null
+  const shadowRootsController = {
+    addShadowRoot() {
+      return null
+    },
+    removeShadowRoot() {
+      return null
+    },
+    flush() {
+      return null
+    },
+    stop() {
+      return null
+    },
   }
-
-  const elementsScrollPositions = createElementsScrollPositions()
-
-  const shadowRootsController = initShadowRootsController(configuration, emitAndComputeStats, elementsScrollPositions)
-
-  const { stop: stopFullSnapshots } = startFullSnapshots(
-    elementsScrollPositions,
-    shadowRootsController,
-    lifeCycle,
-    configuration,
-    flushMutations,
-    (records) => records.forEach((record) => emitAndComputeStats(record))
-  )
-
-  function flushMutations() {
-    shadowRootsController.flush()
-    mutationTracker.flush()
-  }
-
-  const recordIds = initRecordIds()
-  const mutationTracker = trackMutation(emitAndComputeStats, configuration, shadowRootsController, document)
-  const trackers: Tracker[] = [
-    mutationTracker,
-    trackMove(configuration, emitAndComputeStats),
-    trackMouseInteraction(configuration, emitAndComputeStats, recordIds),
-    trackScroll(configuration, emitAndComputeStats, elementsScrollPositions, document),
-    trackViewportResize(configuration, emitAndComputeStats),
-    trackInput(configuration, emitAndComputeStats),
-    trackMediaInteraction(configuration, emitAndComputeStats),
-    trackStyleSheet(emitAndComputeStats),
-    trackFocus(configuration, emitAndComputeStats),
-    trackVisualViewportResize(configuration, emitAndComputeStats),
-    trackFrustration(lifeCycle, emitAndComputeStats, recordIds),
-    trackViewEnd(lifeCycle, (viewEndRecord) => {
-      flushMutations()
-      emitAndComputeStats(viewEndRecord)
-    }),
-  ]
 
   return {
-    stop: () => {
-      shadowRootsController.stop()
-      trackers.forEach((tracker) => tracker.stop())
-      stopFullSnapshots()
-    },
+    stop: stop || (() => undefined),
     flushMutations,
     shadowRootsController,
   }
